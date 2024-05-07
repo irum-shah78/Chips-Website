d5798bb8b3d89aa29629fd46fd3c5b372c7abbde
irum-shah78 <irams4812@gmail.com>
2024-05-07 20:35:19 +0500

    home completed

--- scripts.js
+++ scripts.js
@@ -21,4 +21,8 @@ const linkAction = ()=>{
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-
+const shadowHeader = ()=>{
+  const header = document.getElementById('header');
+  this.scrollY >= 50 ? header.classList.add('shadow-header') : header.classList.remove('shadow-header');
+}
+window.addEventListener('scroll', shadowHeader);
